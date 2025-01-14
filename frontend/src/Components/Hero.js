38d90ef77ce952a2d31a48b20e715618a6954ec3
Mayank Kush <mayankkush1734@gmail.com>
2025-01-14 09:57:54 +0530

    Update Hero.js

--- frontend/src/Components/Hero.js
+++ frontend/src/Components/Hero.js
@@ -18,7 +18,7 @@ const Hero = () => {
       </div>
       <video
         className="hero-video"
-        src="https://youtu.be/GXaMT5pX12w"
+        src="https://imdb-video.media-imdb.com/vi3494298905/1434659607842-pgv4ql-1676535573518.mp4?Expires=1736914700&Signature=IVoXd31w~7UtGslwAHWC03syoivh2nmAPWD6dxoEGbR52FWZSS-sQ400ESHT~p8oP1OrWCbUU~JFsKOaisih7rJU0MpNRsauUdgU4BwsSMiGcKdY8KMZlkeQAvMw1z4hEh5pOcgnAj8Gj-jrguK6xDKyPixE5KxXNNiTklIhx3vC83wkl~EDIu66COd9YJ6JvFJ3qyQ9W7GhfYbGmGGZmzfiJ6voztO3JBzR7np8Yb9OJPkvl27p4Viu5p8oid2PPLRm5RkNtU0ekzHwCznunemdd8WFEwkLbddGsrQAzFMMX94f1-UT7i7iZZxPhaOA0tyOfVIA3Es-OsAixjfFhw__&Key-Pair-Id=APKAIFLZBVQZ24NQH3KA"
         autoPlay
         loop
         muted
